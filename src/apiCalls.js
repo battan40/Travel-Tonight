5dbbc26dfdbb0d3476692ac99c88886e3c5ae70b
Angela Battillo <angela.battillo@gmail.com>
2021-04-23 20:34:32 -0600

    Adjust fetch to include needed data to create remaining class structures

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,22 @@
 import domUpdates from './domUpdates.js';
 import Trip from './Trip.js'
 
-const getAllTravelers () {
+const getAllTravelers() {
   return fetch('http://localhost:3001/api/v1/travelers')
   .then(response => response.json())
   .then(allTravelersData => {
     return allTravelersData.travelers;
   })
   .catch(err => console.log(err.message))
+}
+
+const getAllTrips() {
+  return fetch('http://localhost:3001/api/v1/trips')
+  .then(response => response.json())
+  .then(allTripsData => {
+    return allTripsData.trips;
+  .catch(err => console.log(err.message))
+  })
 }
 
 export { getAllTravelers };
