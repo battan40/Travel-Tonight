e6ace33e7c06fa92187c358492b391f34213240c
Angela Battillo <angela.battillo@gmail.com>
2021-04-23 20:11:48 -0600

    Create basic traveler properties based on data set

--- src/traveler.js
+++ src/traveler.js
@@ -1,4 +1,9 @@
 class Traveler {
+  constructor(travelerData) {
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.type = travelerData.travelerType;
+  }
 
 }
 export default Traveler;
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -4,15 +4,24 @@ import travelerData from './test-data/traveler-data.js'
 import Traveler from '../src/traveler.js'
 
 describe('Traveler', () => {
-  let traveler;
+  let traveler1;
 
   beforeEach(() => {
-    traveler = new Traveler(travelerData[0]);
+    traveler1 = new Traveler(travelerData[0]);
+  });
 
+  it('should be a function', () => {
+      expect(Traveler).to.be.a('function');
   });
 
   it('should instantiate a traveler', () => {
-    expect(traveler).to.be.an.instanceOf(Traveler);
-  })
+    expect(traveler1).to.be.an.instanceOf(Traveler);
+  });
+
+  it('should have an id, name, and traveler type established', ()=> {
+    expect(traveler1.id).to.equal(1);
+    expect(traveler1.name).to.equal('Ham Leadbeater');
+    expect(traveler1.type).to.equal('relaxer');
+  });
 
 });
