88d43acc2ad091e501436be57eed64dd1c33fa37
Angela Battillo <angela.battillo@gmail.com>
2021-04-24 14:17:03 -0600

    Put together a Destination Class to streamline fetch calls

---
+++ test/destination-test.js
@@ -0,0 +1,35 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Destination from '../src/destination.js'
+import destinationData from './test-data/destination-data.js'
+
+
+describe('Destination', () => {
+  let destination;
+
+  beforeEach(() => {
+    destination = new Destination(destinationData[0])
+  });
+
+  it('should be a function', () => {
+      expect(Destination).to.be.a('function');
+  });
+
+  it('should instantiate a destination', () => {
+    expect(destination).to.be.an.instanceOf(Destination);
+
+  });
+
+  it('should have individual aspects of a destination', () => {
+    expect(destination).to.deep.equal(
+    {
+    "id": 1,
+    "destination": "Lima, Peru",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 400,
+    "image": "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
+    "alt": "overview of city buildings with a clear sky"
+    }
+  )
+  });
+});
