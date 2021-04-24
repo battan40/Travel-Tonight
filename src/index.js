b9dbb6736c4a1c243a85cad3b4823b4fdf59e5e7
Angela Battillo <angela.battillo@gmail.com>
2021-04-23 19:16:10 -0600

    Establish import export pathway

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/base.scss';
-
 import './images/turing-logo.png'
+import Traveler from '.traveler.js'
+import getAllTravelers from './apiCalls.js'
+
+let currentTraveler, allTravelers;
+
+window.addEventListener('load', fetchCalls);
+
+function fetchCalls() {
+  apiCalls.getAllTravelers();
+}
