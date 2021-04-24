7eee45f3f8bb458df7a4d1b9b360562694096174
Angela Battillo <angela.battillo@gmail.com>
2021-04-24 14:15:48 -0600

    Add mapping to instantiate individual classes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,12 @@
-
+import Destination from './destination.js'
+import Traveler from './traveler.js'
 import Trip from './trip.js';
 
 const getAllTravelers = () => {
   return fetch('http://localhost:3001/api/v1/travelers')
   .then(response => response.json())
   .then(allTravelersData => {
-    return allTravelersData.travelers;
+    return allTravelersData.travelers.map(traveler => new Traveler(traveler));
   })
   .catch(err => console.log(err.message))
 }
@@ -14,7 +15,7 @@ const getAllTrips = () => {
   return fetch('http://localhost:3001/api/v1/trips')
   .then(response => response.json())
   .then(allTripsData => {
-    return allTripsData.trips;
+    return allTripsData.trips.map(trip => new Trip(trip));
   })
   .catch(err => console.log(err.message))
 }
@@ -23,7 +24,7 @@ const getAllDestinations = () => {
   return fetch('http://localhost:3001/api/v1/destinations')
   .then(response => response.json())
   .then(allDestinationsData => {
-    return allDestinationsData.destinations;
+    return allDestinationsData.destinations.map(destination => new Destination(destination));
   })
   .catch(err => console.log(err.message))
 }
