cf283d12e7df77ef18c6ae2eab92a16f5ae60a1e
Angela Battillo <angela.battillo@gmail.com>
2021-04-23 19:13:02 -0600

    Create initial structure of fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,13 @@
 import domUpdates from './domUpdates.js';
+import Trip from './Trip.js'
 
-export default apiCalls;
+const getAllTravelers () {
+  return fetch('http://localhost:3001/api/v1/travelers')
+  .then(response => response.json())
+  .then(allTravelersData => {
+    return allTravelersData.travelers;
+  })
+  .catch(err => console.log(err.message))
+}
+
+export { getAllTravelers };
