dfdaff1fc87597006122d5b848f358ff253e3278
Angela Battillo <angela.battillo@gmail.com>
2021-04-25 12:53:54 -0600

    Bring in data from index js

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,1 +1,18 @@
+const domUpdates = {
+
+currentTraveler: null,
+today: null,
+allTrips: null,
+allDestinations: null,
+
+applyGlobals(currentTraveler, date, trips, destinations) {
+  this.currentTraveler = currentTraveler;
+  this.today = date;
+  this.allTrips = trips;
+  this.allDestinations = destinations;
+},
+
+
+}
+
 export default domUpdates;
