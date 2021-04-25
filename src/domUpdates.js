fc068fc985124431404bbcd5056e27476ac363da
Angela Battillo <angela.battillo@gmail.com>
2021-04-25 17:42:29 -0600

    Adjust file path for correct post function call and drop down certainty

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -5,11 +5,23 @@ today: null,
 allTrips: null,
 allDestinations: null,
 
-applyGlobals(currentTraveler, date, trips, destinations) {
+accessGlobalInfo(currentTraveler, date, trips, destinations) {
   this.currentTraveler = currentTraveler;
   this.today = date;
   this.allTrips = trips;
   this.allDestinations = destinations;
+},
+
+onLoadFire() {
+  this.displayCurrentTraveler(this.currentTraveler);
+  this.displayTrips(this.currentTraveler, 'upcoming');
+  this.displayExpenses(this.currentTraveler.calculateTripMoneySpentInYear());
+  this.destinationDropDownForBooking();
+},
+
+userFriendlyDateDisplay(date) {
+  let bestDateFormat = new Date(date).toLocaleString();
+  return bestDateFormat;
 },
 
 displayCurrentTraveler(currentTraveler) {
@@ -51,9 +63,51 @@ displayTrips(currentTraveler, tripType) {
     tripDisplay.insertAdjacentHTML('beforeend', tripDetails)
 },
 
-userFriendlyDateDisplay(date) {
-  let bestDateFormat = new Date(date).toLocaleString();
-  return bestDateFormat;
+//Do you need to display filtered trip type?
+
+// bookingButtonChangeUp () {
+//
+// },
+
+destinationDropDownForBooking() {
+  const dropDownList = document.querySelector('#destinationDropDown');
+  this.allDestinations.sort((one, two) => {
+    let destinationOne = one.destination;
+    let destinationTwo = two.destination;
+    if(destinationOne < destinationTwo) {
+      return -1;
+    } else if(destinationTwo > destinationOne) {
+      return 1;
+    } else {
+      return 0
+    }
+  });
+  let locationsToBook = '';
+  this.allDestinations.forEach(destination => {
+    locationsToBook += `<option value="${destination.id}">${destination.destination}</option>`
+  })
+  dropDownList.insertAdjacentHTML('beforeend', locationsToBook)
+},
+
+
+displayCostOfNewTrip() {
+//i'm already doing a lot of this above...
+  const specificDestination = this.allDestinations.find(destination => destination.id === trip.destinationID)
+  const showDate = this.userFriendlyDateDisplay(trip.date);
+//can i find the class bubble event without a parameter?  If so then i can just change the innertext to include:
+//something.classlist.remove('hidden');
+//something.innertext = `Estimated cost for this trip ${trip.cost}`
+//something.classlist.add('book')
+},
+
+resetTripRequestSection() {
+  trip.startDate.value = '';
+  trip.duration.value = '';
+  trip.travelerCount.value = '';
+  trip.destination.value = '';
+  button.classList.remove('book');
+  something.innerText = 'Please make a choice';
+  something.classList.add('hidden');
 },
 
 }
