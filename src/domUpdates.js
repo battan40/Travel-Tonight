114ac4112f713c126b1a7064250d9ad73a919676
Angela Battillo <angela.battillo@gmail.com>
2021-04-25 12:55:06 -0600

    Add inner text to dashboard display for expenses and welcome

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -12,6 +12,18 @@ applyGlobals(currentTraveler, date, trips, destinations) {
   this.allDestinations = destinations;
 },
 
+displayCurrentTraveler(currentTraveler) {
+  const greetTraveler = document.querySelector('.greeting');
+  const divideFullName = currentTraveler.name.split(' ');
+  const name = divideFullName[0];
+  greetTraveler.innerText = `Welcome, ${name}`
+},
+
+displayExpenses() {
+  const yearlyTripExpenses = document.querySelector('#spending');
+  yearlyTripExpenses.innerText = `Total trip expenses this year: $${this.currentTraveler.calculateTripMoneySpentInYear('2020', this.allDestinations)}`
+},
+
 
 }
 
