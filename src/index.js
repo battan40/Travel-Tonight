d16271cbe8607c0c8d040635b4f6c717a6b50747
Angela Battillo <angela.battillo@gmail.com>
2021-04-25 12:59:47 -0600

    Adjust file path between dom updates fetch and index to display dashboard

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './images/TealForestBackground.png';
 import Traveler from './traveler.js';
 import Trip from './trip.js';
 import Destination from './destination.js'
+import domUpdates from './domUpdates.js'
 import { getAllTravelers, getAllTrips, getAllDestinations } from './apiCalls.js';
 
 let currentTraveler, today;
@@ -13,6 +14,7 @@ let allTrips = [];
 let allDestinations = [];
 
 window.addEventListener('load', fetchCalls);
+window.addEventListener('click', displayGetaways)
 
 function fetchCalls() {
   let allFetchData = [
@@ -33,15 +35,37 @@ function fetchCalls() {
       })
       makeTraveler();
       createDate();
+      domUpdates.applyGlobals(currentTraveler, today, allTrips, allDestinations);
+      displayTraveler();
     })
 }
 
 function makeTraveler() {
   currentTraveler.compileAllTrips(allTrips, allDestinations);
   currentTraveler.orderTripsByDate();
-
 }
 
 function createDate() {
   let today = new Date();
 }
+
+function displayTraveler() {
+  domUpdates.displayCurrentTraveler(currentTraveler);
+  domUpdates.displayTrips(currentTraveler, 'upcoming');
+}
+
+function displayGetaways(event){
+  domUpdates.displayTrips(currentTraveler);
+  displayTraveler();
+}
+
+// Before Iteration 2:
+// -Are all trips, past, present, future and upcoming on display on the dom?
+// -is the years expenses displayed on the dom?
+//
+// Iteration 2: Make a Trip Request
+// -select date, numb travelers, choose list of allDestinations
+// -see estimated cost
+// -submit by button click/tab-enter accomodation
+// -type of trip will change from defalut 'upcoming' to spending
+// -add new fetch calls
