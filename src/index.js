cc4e62a1379617ad585ad8a55c13c1a06466b2ce
Angela Battillo <angela.battillo@gmail.com>
2021-04-24 23:02:25 -0600

    Adjust syntax for promise dot all

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
 import './css/base.scss';
-import './images/turing-logo.png';
+import './images/BeachBackGround.png';
+import './images/CliffBackGround.png';
+import './images/TealForestBackground.png';
 import Traveler from './traveler.js';
+import Trip from './trip.js';
+import Destination from './destination.js'
 import { getAllTravelers, getAllTrips, getAllDestinations } from './apiCalls.js';
 
-let currentTraveler, allTravelers;
+let currentTraveler, today;
+let allTravelersData = [];
+let allTrips = [];
+let allDestinations = [];
 
 window.addEventListener('load', fetchCalls);
 
 function fetchCalls() {
-  getAllTravelers();
-  getAllTrips();
-  getAllDestinations();
+  let allFetchData = [
+    getAllTravelers(),
+    getAllTrips(),
+    getAllDestinations()
+  ]
+  Promise.all(allFetchData)
+    .then(data => {
+      currentTraveler = new Traveler(data[0][1]);
+      data[1].forEach(trip => {
+        let vacation = new Trip(trip)
+        allTrips.push(vacation)
+      })
+      data[2].forEach(destination => {
+        let location = new Destination(destination)
+        allDestinations.push(location)
+      })
+      getTraveler()
+    })
+  // getAllTravelers().then(data => {
+  //   currentTraveler = data[0]
+  // }).then(getTraveler());
+  // console.log(allTravelersData)
+  // allTrips = getAllTrips();
+  // allDestinations = getAllDestinations();
+  //getTraveler();
+}
+
+function getTraveler() {
+  //currentTraveler = allTravelers[0]
+  console.log(currentTraveler)
+  console.log(allTrips)
+  console.log(allDestinations)
 }
